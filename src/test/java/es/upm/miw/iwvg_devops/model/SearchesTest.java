1e6425c351c4c1b6a1e210ade1426ac1329fb6ed
Alvaro Tango Fernandez <alvaro.tangofdez@gmail.com>
2021-09-25 10:50:40 +0200

    search number 4 #12

---
+++ src/test/java/es/upm/miw/iwvg_devops/model/SearchesTest.java
@@ -0,0 +1,12 @@
+package es.upm.miw.iwvg_devops.model;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SearchesTest {
+
+    @Test
+    public void testFindFirstDecimalFractionByUserName() {
+        Assertions.assertEquals(2.0, Searches.findFirstDecimalFractionByUserName("Ana"), 10e-5);
+    }
+}
